de722135204d5f4a5d8663c4ae9a2dd42b35e8ec
AbdElkareem134 <1ffffhf@gmail.com>
2025-12-04 15:32:33 +0200

    Refactor main method to include main menu

--- src/App.java
+++ src/App.java
@@ -1,10 +1,70 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println(checkpasswordstrength("Ab1!cdef"));  //8
-        System.out.println(checkpasswordstrength("P@ssw0rd23232")); //10
-        System.out.println(checkpasswordstrength("Abc123!!!")); //(pattern penalty)    }
-    }
+public static void mainMenu()
+	{
+        Scanner in=new Scanner(System.in);
+	System.out.println();
+	System.out.println();
 
+        while(true)
+		{
+			
+			System.out.println("1. Check Password Strength");
+			System.out.println("2. Generate New Password");
+			System.out.println("3. Exit");
+			System.out.print("Choose an option: ");
+            if (!in.hasNextInt())
+            {
+                System.out.println(">> Invalid input, please enter a number.");
+                in.nextLine();
+                continue;
+            }
+			int choice = in.nextInt();
+			in.nextLine();
+			switch (choice)
+			{
+			case 1:
+				System.out.print("Enter password to check: ");
+				String inputPass = in.nextLine();
+				System.out.println(">> Strength Score: " + checkpasswordstrength(inputPass) + "/10");
+				break;
+			case 2:
+				System.out.print("Enter desired length (minimum 8): ");
+				if (in.hasNextInt())
+				{
+					int len = in.nextInt();
+					in.nextLine();
+					if (len < 8)
+					{
+						System.out.println(">> Error: Rules state minimum length is 8 characters.");
+					}
+					else
+					{
+                        System.out.println("here are few passwords:");
+                        for(int i=0;i<5;i++)
+                        {
+                            System.out.println(">> " + generatePassword(len));
+                        }
+					}
+				}
+				else
+				{
+					System.out.println(">> Invalid length!");
+					in.nextLine();
+				}
+				break;
+			case 3:
+				System.out.println("Goodbye!");
+                return;
+			default:
+				System.out.println(">> Invalid choice, please try again.");
+			}
+		}
+	}
+	public static void main(String[] args)
+	{
+		mainMenu();
+
+	}
 
 
 
